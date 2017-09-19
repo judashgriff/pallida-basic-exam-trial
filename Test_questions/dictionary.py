8f21a3fa675c6f2a1b7505c4fa2a5400c59ac9f1
András Krausz <Andras.Krausz@gmail.com>
2017-09-19 15:01:18 +0200

    Feat(dictionary.py): Created new file with basic structure.

---
+++ Test_questions/dictionary.py
@@ -0,0 +1,55 @@
+dictionary = [
+    {"alma": "apple"},
+    {"fa": "tree"}
+]
+
+# Implement this method. It should add the given key-value pair to the
+# list 'dictionary'
+
+
+def querry(user_input):
+    if user_input == 0:
+        hun = input("What is the hungarian version of the word?:\n ")
+        eng = input("What is the english version of the word?:\n ")
+        add_word(hun, eng)
+    elif user_input == 1:
+        question = int(input("If the word is an english word -press \"0\", or if it\'s a hungarian word, - press \"1\" again.\n "))
+        second_level(question)
+    else:
+        print("invalid input. Please enter a \"0\" or a \"1\".")
+    return
+
+
+def second_level(question):
+    if question == 0:
+        eng = input("What is this english word you want to translate?:\n ")
+        translate_to_hun(eng)
+    if question == 1:
+        hun = input("What is this hungarian word you want to translate?:\n")
+        translate_to_eng(hun)
+
+
+def add_word(hun_word, eng_word):
+    dictionary.append({hun_word: eng_word})
+    print(dictionary)
+
+# Implement these methods. They should return the translation of the given
+# word form the list 'dictionary'
+
+
+def translate_to_hun(eng_word):
+    for i in dictionary:
+        if i == eng_word:
+            print(i)
+
+
+def translate_to_eng(hun_word):
+    for i in dictionary:
+        if i == hun_word:
+            print(i)
+
+user_input = int(input("Please press \"0\" and enter if you want to add something to the dictionary, or if you want to translate press \"1\" and enter.:\n "))
+querry(user_input)
+
+
+
