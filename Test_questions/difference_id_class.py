f951740217cbab0bf3e6c1fd53681be114a3e930
András Krausz <Andras.Krausz@gmail.com>
2017-09-19 16:09:53 +0200

    Feat(difference_id_class.py): File is created, the answer is provided to the question in the file.

---
+++ Test_questions/difference_id_class.py
@@ -0,0 +1,10 @@
+What is the difference between id and class in CSS? [2p]
+
+Your answer:
+
+''' One specific id or Identity can be used only once in a html-css file, while classes can be used 
+widely and frequently. For example I can have many items in your html file which have the same class. 
+- for example : "boxes". Thus, if you refer to "boxes", all the items will be affected which have the 
+"boxes" class. Opposingly an id may occur only once in a file: You can have the id called "blue" in your
+html file, so if you refer to this id, you will affect only that one. Still you can have many id-s in 
+your file with different names, but unlike classes, only one item may have the id, not more. '''
