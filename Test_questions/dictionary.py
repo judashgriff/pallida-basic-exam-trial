4d6ad7c4321d89a63be6c7cc906e2d449b6e3895
András Krausz <Andras.Krausz@gmail.com>
2017-09-22 15:21:31 +0200

    Refact(dictionary.py): Fixed previous solution and perfected it. Now it stores data in an outside file, and loads from it.

--- Test_questions/dictionary.py
+++ Test_questions/dictionary.py
@@ -1,13 +1,4 @@
-dictionary = [
-    {"alma": "apple"},
-    {"fa": "tree"}
-]
-
-# Implement this method. It should add the given key-value pair to the
-# list 'dictionary'
-
-
-def querry(user_input):
+def query(user_input):
     if user_input == 0:
         hun = input("What is the hungarian version of the word?:\n ")
         eng = input("What is the english version of the word?:\n ")
@@ -27,33 +18,42 @@ def second_level(question):
     if question == 1:
         hun = input("What is this hungarian word you want to translate?:\n")
         translate_to_eng(hun)
-
+    
 
 def add_word(hun_word, eng_word):
-    dictionary.append({hun_word: eng_word})
-    print(dictionary)
+    with open("dictionary.txt", "a") as f:
+        f.write(hun_word + " : " + eng_word + "\n")
+    return 
 
-# Implement these methods. They should return the translation of the given
-# word form the list 'dictionary'
+
+def import_dict():
+    my_dict = { }
+    with open("dictionary.txt") as f:
+        for line in f:
+            line = line.strip()
+            words = line.split(" : ")
+            my_dict[words[0]] = words[1]
+    return my_dict
 
 
 def translate_to_hun(eng_word):
-    for i in dictionary:
-        key = list(i.keys())[0]
-        if i[key] == eng_word:
-            print(key)
+    my_dict = import_dict()
+    for i in my_dict:
+        if my_dict[i] == eng_word:
+            print(i)
             return
     print("Invalid or not in dictionary. Feel free to enter new values.")
 
 
 def translate_to_eng(hun_word):
-    for i in dictionary:
-        if hun_word == list(i.keys())[0]:
-            print(i[hun_word])
+    my_dict = import_dict()
+    for i in my_dict:
+        if i == hun_word:
+            print(my_dict[i])
             return
     print("Invalid or not in dictionary. Feel free to enter new values.")
 
 
 user_input = int(input("Please press \"0\" and enter if you want to add something to the dictionary, or if you want to translate press \"1\" and enter.:\n "))
 
-querry(user_input)
+query(user_input)
